8629eaf718027f3dc6bc9637717f112dec8912df
Ebrima Singhateh <ebrimasinghateh67@gmail.com>
2025-11-27 19:57:38 +0000

    all caught up

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import reloadIcon from "../assets/image/loading-arrow.png";
+// import reloadIcon from "../assets/image/loading-arrow.png";
 
 const NavBar = ({ onLogoClick }) => {
   return (
